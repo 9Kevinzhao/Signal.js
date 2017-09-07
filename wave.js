37888707b39bfa8afc719fcfb6c6ea4a3cf544f3
9Kevinzhao <9kzhao@stpatrick.org>
2017-09-07 14:55:05 -0500

    Add files via upload

---
+++ wave.js
@@ -0,0 +1,31 @@
+var wave=function(n,p,t,r,pe){
+  var numberOfPoints=p.length;
+  var Points=p;
+  var tranformedPoints=t;
+  var range=r;
+  var Period=p
+}
+
+function getRange(){
+  return range;
+}
+
+function getPeriod(){
+  return Period;
+}
+
+function getPointArray(){
+  return Points;
+}
+
+function shiftX(v){
+  Point.getX()=
+}
+
+function shiftY(v){
+  Point.getY()
+}
+
+function stretchX(v){
+  Points
+}
