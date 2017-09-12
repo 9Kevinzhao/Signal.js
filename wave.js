1f43dd7b3436d5ca249d13b26f3eb42f665cfe2c
9Kevinzhao <9kzhao@stpatrick.org>
2017-09-12 14:40:58 -0500

    Add files via upload

--- wave.js
+++ wave.js
@@ -1,6 +1,8 @@
 const Point = require('./Point.js');
 
-
+let p1;
+let p2;
+let p3;
 var wave=function(array){
   var numberOfPoints=array.length;
   var Points=array;
@@ -22,21 +24,59 @@ function getPointArray(){
 }
 
 function shiftx(v){
-  transformedPoints.getX()=Point.getx+v;
+  for(let i=0;i>numberOfPoints;i++){
+    let oldx=transformedPoints[i].getx();
+    let oldy=transformedPoints[i].gety();
+    oldx = oldx+v;
+    let changedpoint=new Point(oldx,oldy);
+    transformedPoints[i]= changedpoint;
+  }
 }
 
 function shifty(v){
-  transformedPoints.getY()=Point.gety+v
+  for(let i=0;i>numberOfPoints;i++){
+    let oldx=transformedPoints[i].getx();
+    let oldy=transformedPoints[i].gety();
+    oldy = oldy+v;
+    let changedpoint=new Point(oldx,oldy);
+    transformedPoints[i]= changedpoint;
+  }
 }
 
 function stretchx(v){
-  Points.getx()=Point.getx*v
+  for(i=0;i>numberOfPoints;i++){
+    let oldx=transformedPoints[i].getx();
+    let oldy=transformedPoints[i].gety();
+    oldx = oldx*v;
+    let changedpoint=new Point(oldx,oldy);
+    transformedPoints[i]= changedpoint;
+  }
 }
 
 function stretchy(v){
-    Point.gety()=Point.gety*v
+  for(i=0;i>numberOfPoints;i++){
+    let oldx=transformedPoints[i].getx();
+    let oldy=transformedPoints[i].gety();
+    oldy = oldy*v;
+    let changedpoint=new Point(oldx,oldy);
+    transformedPoints[i]= changedpoint;
+  }
 
-transformation(){
+  function transformation(){
+    return transformedPoints
+  }
 
-}
+  function outputOf(xvalue){
+      let reminder= xvalue%(transformedPoints.length
+        return transformedPoints[reminder].gety();
+
+    }
+    else{
+      return "Y not found";
+    }
+  }
+
+  function calculateSpan(array){
+    Math.max(array)-Math.min(array);
+  }
 }
